fedf7b223caf027ec851bf894d5d13e407197097
SantiRosso <santiagorosso@live.com.ar>
2023-02-03 02:48:19 -0300

    login with github

---
+++ src/app/githubLogin.js
@@ -0,0 +1,21 @@
+import {
+  GithubAuthProvider,
+  signInWithPopup,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+import { auth } from "./firebase.js";
+import { showMessage } from "./showMessage.js";
+
+const githubButton = document.querySelector("#githubLogin");
+
+githubButton.addEventListener("click", async () => {
+  const provider = new GithubAuthProvider();
+  try {
+    const credentials = await signInWithPopup(auth, provider);
+    const signinModal = document.querySelector("#signInModal");
+    const modal = bootstrap.Modal.getInstance(signinModal);
+    modal.hide();
+    showMessage("Welcome " + credentials.user.displayName, "success");
+  } catch (error) {
+    showMessage(error.code, "error");
+  }
+});
